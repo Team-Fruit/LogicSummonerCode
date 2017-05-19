18bebb6921a0472e28917e84afe9ee6c6cfc043e
sjcl <fruit@bebehp.com>
2017-05-19 18:41:39 +0900

    #

---
+++ src/旅立ちの街_フンケ/スパイス/ルビーの原石/kaede/Main.java
@@ -0,0 +1,15 @@
+package 旅立ちの街_フンケ.スパイス.ルビーの原石.kaede;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(final String[] args) {
+		final Scanner sc = new Scanner(System.in);
+
+		final int m = sc.nextInt();
+		final int n = sc.nextInt();
+
+		final int x = m*n;
+
+		System.out.println(x);
+	}
+}
