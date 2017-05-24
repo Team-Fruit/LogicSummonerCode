b47f6b3af54cb1fa81c2eb1a103a7e42f89afcbb
bebe0601 <jrhigasinihonn.0601@gmail.com>
2017-05-24 13:41:32 +0900

    Create Main.rb
    魔方陣通った！！！

---
+++ src/旅立ちの街_フンケ/生け贄/夜との契約/sjcl/Main.rb
@@ -0,0 +1,80 @@
+def getHeight(x)
+    array = []
+    for i in 0..@size-1 do
+        array << @magic[i][x]
+    end
+    return array
+end
+
+def getSlant(x)
+    array = []
+    h = @size-1
+    for i in 0..@size-1 do
+        if x == 0 then
+            array << @magic[i][i]
+        elsif x == @size-1 then
+            array << @magic[h][i]
+        end
+        h -= 1
+    end
+    return array
+end
+
+def getSum
+    for i in 0..@size-1 do
+        w = @magic[i]
+        if !w.include?(0) then
+          return w.inject(:+)
+        end
+        h = getHeight(i)
+        if !h.include?(0) then
+            return h.inject(:+)
+        end
+    end
+    ls = getSlant(0)
+    if !ls.include?(0) then
+        return ls.inject(:+)
+    end
+    rs = getSlant(@size-1)
+    if !rs.include?(0) then
+        return rs.inject(:+)
+    end
+end
+
+@size = $stdin.readline.to_i
+@magic = $stdin.read.split(?\n).map(&:split).map! do |line|
+    line.map!(&:to_i)
+end
+sum = getSum
+skipped = false
+loop do
+    b = false
+    for i1 in 0..@magic.length-1 do
+        for i2 in 0..@magic[i1].length-1 do
+            if @magic[i1][i2] == 0 then
+                n = -1
+                if @magic[i1].count(0) <= 1 then
+                    n = sum - @magic[i1].inject(:+)
+                else
+                    h = getHeight(i2)
+                    if h.count(0) <= 1 then
+                        n = sum - h.inject(:+)
+                    end
+                end
+                if n >= 0 then
+                    @magic[i1][i2] = n
+                    if !b then
+                        skipped = true
+                    end
+                else
+                    b = true
+                    skipped = true
+                end
+            end
+        end
+    end
+    break skipped
+end
+@magic.each do |line|
+    puts line.join(" ")
+end
