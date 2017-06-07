8697dd8f88bdbbff6b6cf3c50546ec3d42aee4aa
bebe0601 <jrhigasinihonn.0601@gmail.com>
2017-06-07 10:47:22 +0900

    Update Main.rb

--- src/旅立ちの街_フンケ/生け贄/夜との契約/sjcl/Main.rb
+++ src/旅立ちの街_フンケ/生け贄/夜との契約/sjcl/Main.rb
@@ -7,10 +7,11 @@ def slant(x)
     array = []
     h = @size-1
     for i in 0..@size-1 do
-        if x == 0 then
-            array << @magic[i][i]
-        elsif x == @size-1 then
-            array << @magic[h][i]
+        case x
+            when 0
+                array << @magic[i][i]
+            when @size-1
+                array << @magic[h][i]
         end
         h -= 1
     end
@@ -20,20 +21,20 @@ end
 def sum
     for i in 0..@size-1 do
         w = @magic[i]
-        if !w.include?(0) then
+        if !w.include?(0)
           return w.inject(:+)
         end
         h = @magic.transpose[i]
-        if !h.include?(0) then
+        if !h.include?(0)
             return h.inject(:+)
         end
     end
     ls = slant(0)
-    if !ls.include?(0) then
+    if !ls.include?(0)
         return ls.inject(:+)
     end
     rs = slant(@size-1)
-    if !rs.include?(0) then
+    if !rs.include?(0)
         return rs.inject(:+)
     end
 end
@@ -44,19 +45,19 @@ loop do
     b = false
     for i1 in 0..@magic.length-1 do
         for i2 in 0..@magic[i1].length-1 do
-            if @magic[i1][i2] == 0 then
+            if @magic[i1][i2] == 0
                 n = -1
-                if @magic[i1].count(0) <= 1 then
+                if @magic[i1].count(0) <= 1
                     n = sum - @magic[i1].inject(:+)
                 else
                     h = @magic.transpose[i2]
-                    if h.count(0) <= 1 then
+                    if h.count(0) <= 1
                         n = sum - h.inject(:+)
                     end
                 end
-                if n >= 0 then
+                if n >= 0
                     @magic[i1][i2] = n
-                    if !b then
+                    if !b
                         skipped = true
                     end
                 else
